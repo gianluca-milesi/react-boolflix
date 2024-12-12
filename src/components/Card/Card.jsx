8fae5408d7adb8fa3af0abdd3cda38987aad2385
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-12-12 16:49:51 +0100

    ADD: style to Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,17 +3,22 @@ import style from "./Card.module.css"
 function Card({ item = {} }) {
 
     const { poster_path, title, original_title, original_language, vote_average } = item
+    const pathImage = "https://image.tmdb.org/t/p/w500"
 
     return (
-        <div className="card">
+        <div className={style.card}>
             <figure>
-                <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} />
+                <img className={style.film_image} src={`${pathImage}${poster_path}`} />
             </figure>
-            <div className="card_body">
-                <h3>{title}</h3>
-                <h4>{original_title}</h4>
-                <p>{original_language}</p>
-                <p>{vote_average}</p>
+            <div className={style.card_body}>
+                <div className={style.titles}>
+                    <h3>{title}</h3>
+                    <h5>{original_title}</h5>
+                </div>
+                <div className={style.details}>
+                    <p><span>Language: </span>{original_language}</p>
+                    <p><span>Vote: </span>{vote_average}</p>
+                </div>
             </div>
         </div>
     )
